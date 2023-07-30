e753fac9b67b63eff3ed561f51656f9f1703603b
dielawn <dielawnmerkill@gmail.com>
2023-07-30 06:13:02 -0600

    starting over with insert function

--- script.js
+++ script.js
@@ -17,38 +17,61 @@ const getRandomNumbers = (numberOfNumbers) => {
 
 function mergeSort(arr) {
     if (arr.length <= 1) {
-        return arr;
+        return arr
     }
 
     const mid = Math.floor(arr.length / 2);
-    const leftArr = arr.slice(0, mid);
-    const rightArr = arr.slice(mid);
+    const leftArr = arr.slice(0, mid)
+    const rightArr = arr.slice(mid)
 
-    const sortedLeft = mergeSort(leftArr);
-    const sortedRight = mergeSort(rightArr);
+    const sortedLeft = mergeSort(leftArr)
+    const sortedRight = mergeSort(rightArr)
 
-    return merge(sortedLeft, sortedRight);
+    return merge(sortedLeft, sortedRight)
 }
 
 function merge(left, right) {
-    let mergedArr = [];
-    let leftIndex = 0;
-    let rightIndex = 0;
+    let mergedArr = []
+    let leftIndex = 0
+    let rightIndex = 0
 
     while (leftIndex < left.length && rightIndex < right.length) {
         if (left[leftIndex] < right[rightIndex]) {
-            mergedArr.push(left[leftIndex]);
+            if (mergedArr.length === 0 || left[leftIndex] !== mergedArr[mergedArr.length - 1]) {
+                mergedArr.push(left[leftIndex]);
+            }
             leftIndex++;
+        } else if (right[rightIndex] < left[leftIndex]) {
+            if (mergedArr.length === 0 || right[rightIndex] !== mergedArr[mergedArr.length - 1]) {
+                mergedArr.push(right[rightIndex]);
+            }
+            rightIndex++;
         } else {
-            mergedArr.push(right[rightIndex]);
+            // If both elements are equal, push one of them (avoid duplicates)
+            if (mergedArr.length === 0 || left[leftIndex] !== mergedArr[mergedArr.length - 1]) {
+                mergedArr.push(left[leftIndex]);
+            }
+            leftIndex++;
             rightIndex++;
         }
+        // Add remaining elements from both arrays
+    while (leftIndex < left.length) {
+        if (mergedArr.length === 0 || left[leftIndex] !== mergedArr[mergedArr.length - 1]) {
+            mergedArr.push(left[leftIndex]);
+        }
+        leftIndex++;
     }
 
+    while (rightIndex < right.length) {
+        if (mergedArr.length === 0 || right[rightIndex] !== mergedArr[mergedArr.length - 1]) {
+            mergedArr.push(right[rightIndex]);
+        }
+        rightIndex++;
+    }
     return mergedArr.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+    }
 }
-
-const randomArray = getRandomNumbers(25)
+const randomArray = getRandomNumbers(50)
 
 
 
@@ -64,47 +87,81 @@ class Node {
 }
 
 //build a tree class *accepts array, root attributes uses return value of buildTree()
-class Tree {
-    constructor(array) {
-        this.root = this.buildTree(array)
-        this.size = 0
-    }
+// class Tree {
+//     constructor(array) {
+//         this.root = this.buildTree(array)
+//         this.size = 0
+//         array.forEach((value) => this.insert(value))
+//     }
 
 //buildTree function takes array turns it into an balanced binary tree full of Node objects *sort and remove duplicates, return the level-0 root node
-    buildTree(array) {
-        const sortedArray = mergeSort(array)
-
-        if (sortedArray.length <= 0) {
-            return null // Base case: no elements to create a node, return null
-        }
-
-        const rootNodeIndex = Math.floor(sortedArray.length / 2)
-        const root = new Node(sortedArray[rootNodeIndex])
-
-        root.left = this.buildTree(sortedArray.slice(0, rootNodeIndex))
-        root.right = this.buildTree(sortedArray.slice(rootNodeIndex + 1))
-
-        return root
+class Tree {
+    constructor() {
+        this.root = null    
     }
+    returnData(array) {
+        array.forEach(item => {
+            return item
+        });
+    }
+        
+    insert(data) {
+        let newNode = new Node(data)
+        if (this.root === null) {
+            this.root = newNode
+        } else {
+            this.insertNode(this.root, newNode)
+        }
+    }
+    insertNode(node, newNode) {
+        if (newNode.data < node.data) {
+            if (node.left === null) {
+                node.left= newNode
+            } else {
+                this.insertNode(node.left, newNode)
+            }
+        } else {
+            if (node.right === null) {
+                node.right = newNode
+            } else {
+                this.insertNode(node.right, newNode)
+            }
+        }
+    }
+
     prettyPrint(node = this.root, prefix = "", isLeft = true) {
         if (node === null) {
-            return;
+            return
         }
 
         if (node.right !== null) {
-            this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+            this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false)
         }
 
-        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`); // Updated from node.data to node.value
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`)
        
         if (node.left !== null) {
-            this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+            this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
         }
     }
-
-
 //write insert and delete functions which accepts a value
+// insert(value, root = this.root) {
+//     if (root === null) {
+//       this.size++;
+//       return new Node(value);
+//     }
+  
+//     if (value < root.value) {
+//       root.left = this.insert(value, root.left);
+//     } else if (value > root.value) {
+//       root.right = this.insert(value, root.right);
+//     }
+  
+//     return root;
+//   }
+    removeFrom(index) {
 
+    }
 //find function which accepts a value and return the node with the given value
 
 //level order function accepts another fuction, traverses the tree, returns an array of values, queue array
@@ -135,8 +192,9 @@ console.log(myTree)
 
 
 // Test the function
-const array = randomArray
-const root = myTree.buildTree(array);
+
+const root = myTree.insert(myTree.returnData(randomArray))
+// const insert = myTree.insert(99)
 const print = myTree.prettyPrint(root)
 
 console.log(root); // Print the value of the level-0 root node
